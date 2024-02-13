bfde35452d268443ea9a087062437de33a28befd
ssrinath-sri <ssrinath.sri@gmail.com>
2024-02-13 15:51:00 +0530

    renamed set title

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
-    .setTitle('Cats example')
+    .setTitle('NestJs example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
